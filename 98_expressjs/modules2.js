46cf7815710629dd22bc91dea18046cd14df3998
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2023-04-16 14:08:03 +0530

    Serving HTML files using Expressjs

---
+++ 98_expressjs/modules2.js
@@ -0,0 +1,13 @@
+//ES6
+
+export const hello = () => {
+    console.log("Hello form modules 2");
+}
+
+export const Ahello = (name) => {
+    console.log("Hello  " + name);
+}
+const shubhuji = () => {
+    console.log("Hello shubhanshu ji");
+}
+export default shubhuji;
