17afe499508db10f0f3f64c55f297a60fa83051d
Shubhanshu Nishad <69467788+Shubhanshu-Nishad@users.noreply.github.com>
2023-04-16 12:41:18 +0530

    Modules tuts

---
+++ 97_modulesjs/97_modules.js
@@ -0,0 +1,25 @@
+// const hello = require("./modules1");
+// hello.hello();
+// hello.Ahello("Shubhanshu");
+// hello.Ahello("Sdfd");
+// hello.Ahello("Shubhanshu");
+
+
+//common js 
+
+
+
+// const { hello, Ahello } = require("./modules1");
+// hello();
+// Ahello("Shubhanshu");
+// Ahello("Sdfd");
+// Ahello("Shubhanshu");
+
+
+//ES6
+import shubhuji, { hello, Ahello } from "./modules2.js"
+hello();
+Ahello("Shubhanshu");
+Ahello("Sdfd");
+Ahello("Shubhanshu");
+shubhuji();
---
+++ 97_modulesjs/modules1.js
@@ -0,0 +1,8 @@
+const hello = () => {
+    console.log("Hello form modules 1");
+}
+
+const Ahello = (name) => {
+    console.log("Hello form modules 1" + name);
+}
+module.exports = { hello, Ahello };
